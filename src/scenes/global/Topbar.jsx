b4ed59c8ae2ae765b42300bb5c8983d10fbc6191
isabelchua <isabelchua86@gmail.com>
2023-09-15 11:12:38 -0700

    save 9-15

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,24 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlinedIcon";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlinedIcon";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlinedIcon";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlinedIcon";
+import SearchIcon from "@mui/icons-material/SearchIcon";
+
 const Topbar = () => {
-	return <div>Topbar</div>;
+	const theme = useTheme();
+	const colors = tokens(theme.pallete.mode);
+	const colorMode = useContext(ColorModeContext);
+
+	return (
+		<Box display="flex" justifyContent="space-between" p={2}>
+			Topbar
+		</Box>
+	);
 };
 
 export default Topbar;
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
+import { tokens } from "../../theme";
 
 // color design tokens
 export const token = mode => ({
